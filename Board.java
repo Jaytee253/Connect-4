04923642f679d2fd88655a787e2a48bf9a2345aa
James Rea <jaytee253@icloud.com>
2021-03-23 09:23:51 -0700

    create board. add file src.

--- Board.java
+++ Board.java
@@ -1,5 +1,37 @@
 public class Board {
+
+    public boolean[] columnStatus = new boolean[7];
+    public char[] column = new char[7];
+    public boolean[] rowStatus = new boolean [6];
+    public char[] row = new char[6];
+
+    public static void setBoard(boolean[] columnStatus, char[] column, boolean[] rowStatus, char[] row){
+        for (int i = 0; i < 8; i++){
+            columnStatus[i] = true;
+        }
+
+        for (int i = 0; i < 7; i++){
+            rowStatus[i] = true;
+        }
+
+        for (int i = 0; i < 8; i++){
+            column[i] = ' ';
+        }
+
+        for (int i = 0; i < 7; i++){
+            row[i] = ' ';
+        }
+    }
     
-    
+
+    public static void createRow(char[] column){
+        for (int i = 0; i < 8; i++){
+            System.out.print(column[i] + "|");
+        }
+    }
+
+    public static void createBoard(char[] columnMarkers){
+        
+    }
 
 }
--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
-public final class Main {
+import java.util.Scanner;
+public final class Main extends Board {
+    Scanner userInput = new Scanner(System.in);
+
     
+
+
 }
